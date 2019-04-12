235b974cc8aaf86edcbce0f8b944fcd4dfbdd2b1
dags- <dags@dags.me>
2019-04-12 08:54:29 +0100

    - revert back to PlayerLookup - throw more specific UnsupportedPlatformException - catch UnsupportedPlatformException and print during find()

--- src/main/java/com/mikeprimm/schematicbrush/PlayerLookup.java
+++ src/main/java/com/mikeprimm/schematicbrush/PlayerLookup.java
@@ -10,21 +10,22 @@ import com.sk89q.worldedit.extent.inventory.BlockBag;
 import com.sk89q.worldedit.session.SessionKey;
 import com.sk89q.worldedit.util.Location;
 import com.sk89q.worldedit.world.World;
+
+import javax.annotation.Nullable;
 import java.util.Optional;
 import java.util.UUID;
-import javax.annotation.Nullable;
 
 /**
  * Used to retrieve the (WE)Platform's implementation of the (WE)Player type
  * Assumes that Platform.matchPlayer(..) relies on either the name (String) or id (UUID) of the player
  * Any other method call will result in an UnsupportedOperationException
  */
-public class PlayerMatcher extends AbstractPlayerActor {
+public class PlayerLookup extends AbstractPlayerActor {
 
     private final String name;
     private final UUID id;
 
-    private PlayerMatcher(String name, UUID id) {
+    private PlayerLookup(String name, UUID id) {
         this.name = name;
         this.id = id;
     }
@@ -41,94 +42,94 @@ public class PlayerMatcher extends AbstractPlayerActor {
 
     @Override
     public World getWorld() {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public int getItemInHand() {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public void giveItem(int i, int i1) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public BlockBag getInventoryBlockBag() {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public WorldVector getPosition() {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public double getPitch() {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public double getYaw() {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public void setPosition(Vector vector, float v, float v1) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Nullable
     @Override
     public BaseEntity getState() {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public Location getLocation() {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public void printRaw(String s) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public void printDebug(String s) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public void print(String s) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public void printError(String s) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public SessionKey getSessionKey() {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Nullable
     @Override
     public <T> T getFacet(Class<? extends T> aClass) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public String[] getGroups() {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     @Override
     public boolean hasPermission(String s) {
-        throw new UnsupportedOperationException();
+        throw new UnsupportedPlatformException();
     }
 
     /**
@@ -138,13 +139,20 @@ public class PlayerMatcher extends AbstractPlayerActor {
      * @return The WorldEdit Player or empty if not found
      */
     public static Optional<Player> find(String name, UUID uuid) {
-        PlayerMatcher matcher = new PlayerMatcher(name, uuid);
-        Player result = WorldEdit.getInstance().getServer().matchPlayer(matcher);
-        // If null or our PlayerMatcher instance is returned then assume that the Player was not found
-        if (result == null || result == matcher) {
+        try {
+            PlayerLookup matcher = new PlayerLookup(name, uuid);
+            Player result = WorldEdit.getInstance().getServer().matchPlayer(matcher);
+            // If null or our PlayerMatcher instance is returned then assume that the Player was not found
+            if (result == null || result == matcher) {
+                return Optional.empty();
+            }
+            return Optional.of(result);
+        } catch (UnsupportedPlatformException e) {
+            // Thrown if matchPlayer() for some reason calls any other method on PlayerMatcher
+            // Shouldn't happen on current WE implementations but maybe break in future
+            e.printStackTrace();
             return Optional.empty();
         }
-        return Optional.of(result);
     }
 
     /**
@@ -153,6 +161,18 @@ public class PlayerMatcher extends AbstractPlayerActor {
      * @return The WorldEdit Player or empty if not found
      */
     public static Optional<Player> find(org.spongepowered.api.entity.living.player.Player player) {
-        return PlayerMatcher.find(player.getName(), player.getUniqueId());
+        return PlayerLookup.find(player.getName(), player.getUniqueId());
+    }
+    
+    private static class UnsupportedPlatformException extends RuntimeException {
+
+        private UnsupportedPlatformException() {
+            super(String.format(
+                    "SchematicBrush does not support this platform: WorldEdit Version=%s, Platform Name=%s, Platform Version=%s",
+                    WorldEdit.getVersion(),
+                    WorldEdit.getInstance().getServer().getPlatformName(),
+                    WorldEdit.getInstance().getServer().getPlatformVersion()
+            ));
+        }
     }
 }
